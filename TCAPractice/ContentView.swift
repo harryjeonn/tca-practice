801ca94946e8bafc46d1f18d12c2d540214aa5a0
Harry <jeongus97@gmail.com>
2022-08-25 10:52:44 +0900

    Create Project

---
+++ TCAPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TCAPractice
+//
+//  Created by Harry on 2022/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCAPractice/TCAPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPracticeApp.swift
+//  TCAPractice
+//
+//  Created by Harry on 2022/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TCAPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
